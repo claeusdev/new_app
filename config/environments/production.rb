1e0193a3e5e9fcfe8bf00e6f3e3be00982e4e6e8
Nana Adjei Manu <claeusdev@gmail.com>
2017-11-24 01:49:30 +0000

    Debugging fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
 
   # Settings specified here will take precedence over those in config/application.rb.
