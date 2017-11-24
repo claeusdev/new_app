65811f986511ee19786e5cf76309cb46d5c3757c
Nana Adjei Manu <claeusdev@gmail.com>
2017-11-24 01:13:05 +0000

    Changed font settings:

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Pay
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.embed
+    config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
